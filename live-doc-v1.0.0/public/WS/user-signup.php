dc4fc4179e40551306361bea44bfe6a50a85a890
kenn0808 <kenkwando08@gmail.com>
2023-06-09 14:35:00 +0700

    update ws user-sign-up

--- live-doc-v1.0.0/public/WS/user-signup.php
+++ live-doc-v1.0.0/public/WS/user-signup.php
@@ -1,49 +1,56 @@
-<?php 
-	header("Access-Control-Allow-Origin: *");
-	error_reporting(E_ERROR | E_PARSE);
-	clearstatcache();
-	$conn = new mysqli("localhost", "root", "", "dbmagang");
+<?php
+header("Access-Control-Allow-Origin: *");
+error_reporting(E_ERROR | E_PARSE);
+clearstatcache();
+$conn = new mysqli("localhost", "root", "", "dbmagang");
 
 
-	$email = $_POST['email'];
-	$nama = $_POST['nama'];
-	$password = $_POST['password'];
-	$nomorTelepon = $_POST['nomorTelepon'];
-	$repeatPassword = $_POST['repeatPassword'];
+$email = $_POST['email'];
+$nama = $_POST['nama'];
+$password = $_POST['password'];
+$nomorTelepon = $_POST['nomorTelepon'];
+$repeatPassword = $_POST['repeatPassword'];
 
-	$arr=null;
+$arr = null;
 
-	if($conn->connect_error) {
-    	$arr= ["result"=>"error","message"=>"Error Connect DB"];
-	}
-	else{
-	    $sql1 = "SELECT * FROM penggunas where nomor_telepon=?";
+if ($conn->connect_error) {
+	$arr = ["result" => "error", "message" => "Error Connect DB"];
+} else {
+	$sql1 = "SELECT id FROM penggunas where nomor_telepon=?";
+	$stmt = $conn->prepare($sql1);
+	$stmt->bind_param("s", $nomorTelepon);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if ($result->num_rows > 0) {
+		$arr = ["result" => "error", "message" => "Nomor telepon sudah pernah digunakan, silahkan gunakan nomor telepon lain untuk Sign Up"];
+	} else {
+		$sql1 = "SELECT id FROM penggunas where email=?";
 		$stmt = $conn->prepare($sql1);
-		$stmt->bind_param("s",$nomorTelepon);
+		$stmt->bind_param("s", $email);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if ($result->num_rows > 0) {
-			$arr= ["result"=>"error","message"=>"Nomor telepon sudah pernah digunakan, silahkan gunakan nomor telepon lain untuk Sign Up"];
-		} 
-		else {
+			$arr = ["result" => "error", "message" => "Email sudah pernah digunakan, silahkan gunakan Email lain untuk Sign Up"];
+		} else {
 			if ($password == $repeatPassword) {
 				$sql = "INSERT into penggunas(nama,nomor_telepon, email, password) VALUES(?,?,?,?)";
-				    $stmt = $conn->prepare($sql);
-					$stmt->bind_param("ssss",$nama, $nomorTelepon, $email, $password);
-					$stmt->execute();
-					
+				$stmt = $conn->prepare($sql);
+				$stmt->bind_param("ssss", $nama, $nomorTelepon, $email, $password);
+				$stmt->execute();
 
-					if ($stmt->affected_rows > 0) {
-						$arr=["result"=>"success"];
-					} 
-					else {
-						$arr= ["result"=>"error","message"=>"sql error: $sql"];
-					}
-			}else{
-				$arr= ["result"=>"error","message"=>"Pastikan password yang dimasukkan sama"];
+
+				if ($stmt->affected_rows > 0) {
+					$arr = ["result" => "success"];
+				} else {
+					$arr = ["result" => "error", "message" => "sql error: $sql"];
+				}
+			} else {
+				$arr = ["result" => "error", "message" => "Pastikan password yang dimasukkan sama"];
 			}
 		}
 	}
-	echo json_encode($arr);
- ?>
+}
+echo json_encode($arr);
+?>
